4777d179afb32dc0173c877b0c0f699b79be9f13
JensDiemer <git@jensdiemer.de>
2013-07-23 17:54:56 +0200

    from django.conf.urls.defaults import... -> from django.conf.urls import...

--- reversion_compare_test_project/urls.py
+++ reversion_compare_test_project/urls.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
