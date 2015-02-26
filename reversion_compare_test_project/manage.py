d63c3273d2bca4c7cecfc7e175fd5cfae69ebbd9
theodore.therone@gmail.com <aristotle@ip-10-155-13-24.ec2.internal>
2015-02-26 11:55:13 +0000

    rewrote test manage.py in 'the new way'
    Execute manager deprecated, See: http://stackoverflow.com/questions/20906305

--- reversion_compare_test_project/manage.py
+++ reversion_compare_test_project/manage.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import os
+import os,sys
+
+BASE = os.path.dirname(os.path.dirname(__file__))
+sys.path.insert(1,os.path.join(BASE, "../"))
 
 os.environ['DJANGO_SETTINGS_MODULE'] = "reversion_compare_test_project.settings"
 
-from django.core.management import execute_manager
-import imp
-try:
-    imp.find_module('settings') # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
-    sys.exit(1)
-
-import settings
-
 if __name__ == "__main__":
-    execute_manager(settings)
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
