1d0e1a4666bb7741dff717971714a0ac3c07234e
Jens Diemer <git@jensdiemer.de>
2015-07-13 13:41:55 +0200

    change setup.py and release as 0.5.3

--- reversion_compare/__init__.py
+++ reversion_compare/__init__.py
@@ -1,11 +1,3 @@
 # coding: utf-8
 
-
-__version__ = (0, 5, 2)
-
-
-VERSION_STRING = '.'.join(str(part) for part in __version__)
-
-
-if __name__ == "__main__":
-    print(VERSION_STRING)
+__version__ = "0.5.3"
--- setup.py
+++ setup.py
@@ -17,19 +17,68 @@ import sys
 
 from setuptools import setup, find_packages
 
-from reversion_compare import VERSION_STRING
+from reversion_compare import __version__
 
 
 if "publish" in sys.argv:
-    import subprocess
-    args = [sys.executable or "python", "setup.py", "sdist", "bdist_wheel", "upload"]
-    print("\nCall: %r\n" %  " ".join(args))
-    subprocess.call(args)
+    try:
+        # Test if wheel is installed, otherwise the user will only see:
+        #   error: invalid command 'bdist_wheel'
+        import wheel
+    except ImportError as err:
+        print("\nError: %s" % err)
+        print("\nMaybe https://pypi.python.org/pypi/wheel is not installed or virtualenv not activated?!?")
+        print("e.g.:")
+        print("    ~/your/env/$ source bin/activate")
+        print("    ~/your/env/$ pip install wheel")
+        sys.exit(-1)
 
-    print("\nDon't forget to tag this version, e.g.:")
-    print("\tgit tag v%s" % VERSION_STRING)
-    print("\tgit push --tags")
-    sys.exit()
+    import subprocess
+
+    def verbose_check_output(*args):
+        print("\nCall: %r\n" %  " ".join(args))
+        try:
+            return subprocess.check_output(args, universal_newlines=True)
+        except subprocess.CalledProcessError as err:
+            print("\n***ERROR:")
+            print(err.output)
+            raise
+
+    def verbose_check_call(*args):
+        print("\nCall: %r\n" %  " ".join(args))
+        subprocess.check_call(args, universal_newlines=True)
+
+    # Check if we are on 'master' branch:
+    output = verbose_check_output("git", "branch", "--no-color")
+    if "* master" in output:
+        print("OK")
+    else:
+        print("\nNOTE: It seems you are not on 'master':")
+        print(output)
+        if input("\nPublish anyhow? (Y/N)").lower() not in ("y", "j"):
+            print("Bye.")
+            sys.exit(-1)
+
+    # publish only if git repro is clean:
+    output = verbose_check_output("git", "status", "--porcelain")
+    if output == "":
+        print("OK")
+    else:
+        print("\n***ERROR: git repro not clean:")
+        print(output)
+        sys.exit(-1)
+
+    # tag first (will raise a error of tag already exists)
+    verbose_check_call("git", "tag", "v%s" % __version__)
+
+    # build and upload to PyPi:
+    verbose_check_call(sys.executable or "python", "setup.py", "sdist", "bdist_wheel", "upload")
+
+    # push
+    verbose_check_call("git", "push")
+    verbose_check_call("git", "push", "--tags")
+
+    sys.exit(0)
 
 
 PACKAGE_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -58,7 +107,7 @@ def get_authors():
 
 setup(
     name='django-reversion-compare',
-    version=VERSION_STRING,
+    version=__version__,
     description='history compare for django-reversion',
     keywords=["django", "django-reversion", "reversion", "diff", "compare"],
     long_description=long_description,
