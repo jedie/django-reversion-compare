b66598623935a132433da9631a00567b59a2600a
JensDiemer <git@jensdiemer.de>
2016-08-29 17:10:52 +0200

    v0.7.1 is not ready for django v1.10

--- setup.py
+++ setup.py
@@ -205,7 +205,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "Django>=1.8",
+        "Django>=1.8,<1.10",
         "django-reversion>=2.0,<2.1",
     ],
     tests_require=[
