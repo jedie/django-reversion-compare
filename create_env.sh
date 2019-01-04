f2521ff4d55f36e0eb57e89629cb403ce22662ad
JensDiemer <git@jensdiemer.de>
2019-01-04 13:01:41 +0100

    add 'create_env.sh' for easier create a dev. virtualenv

---
+++ create_env.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+DESTINATION=$(pwd)/.virtualenv
+REQ_FILE=$(pwd)/requirements-dev.txt
+
+(
+    set -e
+    set -x
+
+    python3 --version
+    python3 -Im venv --without-pip ${DESTINATION}
+)
+(
+    source ${DESTINATION}/bin/activate
+    set -x
+    python3 -m ensurepip
+)
+if [ "$?" == "0" ]; then
+    echo "pip installed, ok"
+else
+    echo "ensurepip doesn't exist, use get-pip.py"
+    (
+        set -e
+        source ${DESTINATION}/bin/activate
+        set -x
+        cd ${DESTINATION}/bin
+        wget https://bootstrap.pypa.io/get-pip.py
+        ${DESTINATION}/bin/python get-pip.py
+    )
+fi
+(
+    set -e
+    source ${DESTINATION}/bin/activate
+    set -x
+    cd ${DESTINATION}/bin/
+
+    pip3 install --upgrade pip
+    pip3 install -r ${REQ_FILE}
+)
