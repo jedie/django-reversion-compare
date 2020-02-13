739b07e6d819b815cd45e52680aaaf2ff44167b2
JensDiemer <git@jensdiemer.de>
2020-02-13 15:27:09 +0100

    update test project settings

--- reversion_compare_tests/settings.py
+++ reversion_compare_tests/settings.py
@@ -1,16 +1,42 @@
 import os
 import tempfile
+from pathlib import Path
+
+print("Use settings:", __file__)
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = Path(__file__).parent
 
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {"context_processors": ["django.contrib.auth.context_processors.auth"]},
+        "DIRS": [Path(BASE_DIR, "templates")],
+        "OPTIONS": {
+            "loaders": [
+                (
+                    "django_tools.template.loader.DebugCacheLoader",
+                    (
+                        "django.template.loaders.filesystem.Loader",
+                        "django.template.loaders.app_directories.Loader"
+                    ),
+                )
+            ],
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.template.context_processors.media",
+                "django.template.context_processors.csrf",
+                "django.template.context_processors.tz",
+                "django.template.context_processors.static",
+            ],
+        },
     }
 ]
 
-MIDDLEWARE_CLASSES = MIDDLEWARE = (
+MIDDLEWARE = (
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -33,6 +59,7 @@ INSTALLED_APPS = [
     "django.contrib.sites",
     "django.contrib.admin",
     "django.contrib.sessions",
+    "django.contrib.messages",
     "reversion",
     "reversion_compare",
     "reversion_compare_tests",
@@ -42,6 +69,7 @@ ROOT_URLCONF = "reversion_compare_tests.urls"
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 
+# The temp path used for e.g.: models.FilePathField()
 try:
     UNITTEST_TEMP_PATH = os.environ["UNITTEST_TEMP_PATH"]
 except KeyError:
@@ -52,7 +80,12 @@ except KeyError:
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(UNITTEST_TEMP_PATH, "reversion_compare_unitfixturesbase"),
+        "NAME": str(Path(BASE_DIR.parent, "test_project_db.sqlite3")),
+        # 'NAME': ":memory:",
+        'OPTIONS': {
+            # https://docs.djangoproject.com/en/2.2/ref/databases/#database-is-locked-errors
+            'timeout': 20,
+        }
     }
 }
 
