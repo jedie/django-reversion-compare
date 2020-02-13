b6196e51bb90fbe820f768e9b2d13106dd2b59ac
JensDiemer <git@jensdiemer.de>
2020-02-13 15:27:34 +0100

    bugfix import

--- reversion_compare_tests/manage.py
+++ reversion_compare_tests/manage.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
 import sys
 
-from .utils import cleanup_temp
+from reversion_compare_tests.utils import cleanup_temp
 
 
 def cli(arguments):
