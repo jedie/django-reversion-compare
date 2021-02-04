418dfedeeeed2a592607b0b9b39d03ce86543abf
JensDiemer <git@jensdiemer.de>
2021-02-04 11:46:45 +0100

    Don't use DebugCacheLoader

--- reversion_compare_tests/settings.py
+++ reversion_compare_tests/settings.py
@@ -14,13 +14,6 @@ TEMPLATES = [
         "DIRS": [Path(BASE_DIR, "templates")],
         "OPTIONS": {
             "loaders": [
-            #     (
-            #         "django_tools.template.loader.DebugCacheLoader",
-            #         (
-            #             "django.template.loaders.filesystem.Loader",
-            #             "django.template.loaders.app_directories.Loader"
-            #         ),
-            #     )
                 "django.template.loaders.filesystem.Loader",
                 "django.template.loaders.app_directories.Loader"
             ],
