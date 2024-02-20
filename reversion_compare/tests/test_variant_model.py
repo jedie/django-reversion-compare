f6ba3edcc154f758b73036f5d09eaa19861150a7
JensDiemer <git@jensdiemer.de>
2024-02-20 09:22:45 +0100

    Enhance diff
    * Remove Django 4.1 tests and add 5.0
    * Remove Python 3.9 from test matrix (because Django 5.0 doesn't support it)
    * Use `pretty_repr` from `rich` in `CompareObject`
    * Modernize CSS styles and support dark theme
    * Add `JSONField` to `VariantModel` test model

--- reversion_compare/tests/test_variant_model.py
+++ reversion_compare/tests/test_variant_model.py
@@ -13,8 +13,7 @@
     :copyleft: 2012-2022 by the django-reversion-compare team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
-
-
+from bx_django_utils.test_utils.html_assertion import assert_html_response_snapshot, get_django_name_suffix
 from override_storage import locmem_stats_override_storage
 from override_storage.utils import Stats
 from reversion import create_revision, is_registered
@@ -76,10 +75,11 @@ class VariantModelWithDataTest(BaseTestCase):
 
         self.item, self.fixtures = Fixtures(verbose=False).create_VariantModel_data()
 
-        assert dict(self.storage_stats.saves_by_field) == {
-            ('reversion_compare_project', 'variantmodel', 'file_field'): 'file_field_18.txt'
-        }
-        assert self.storage_stats.save_cnt == 20
+        self.assertEqual(
+            dict(self.storage_stats.saves_by_field),
+            {('reversion_compare_project', 'variantmodel', 'file_field'): 'file_field_19.txt'},
+        )
+        self.assertEqual(self.storage_stats.save_cnt, 21)
 
         queryset = Version.objects.get_for_object(self.item)
         self.version_ids = queryset.values_list("pk", flat=True)
@@ -105,77 +105,88 @@ class VariantModelWithDataTest(BaseTestCase):
         self.assertEqual(response.status_code, 200, response)
 
         field_headlines = [f"<h3>{field_name.replace('_', ' ')}</h3>" for field_name, value in self.fixtures]
-        self.assertContainsHtml(response, *field_headlines)
-        self.assertContainsHtml(
+        self.assert_html_parts(response, parts=field_headlines)
+        self.assert_html_parts(
             response,
-            "<h3>boolean</h3>",
-            '<p class="highlight"><del>False</del> changed to: <ins>True</ins></p>',
-            "<h3>null boolean</h3>",
-            '<p class="highlight"><del>None</del> changed to: <ins>False</ins></p>',
-            "<h3>char</h3>",
-            "<del>- a</del>",
-            "<ins>+ B</ins>",
-            "<h3>choices char</h3>",
-            "<del>- alpha</del>",
-            "<ins>+ bravo</ins>",
-            "<h3>text</h3>",
-            "<del>- Foo &#39;one&#39;</del>",
-            "<ins>+ Bar &#39;two&#39;</ins>",
-            "<h3>integer</h3>",
-            "<del>- 0</del>",
-            "<ins>+ -1</ins>",
-            "<h3>integers</h3>",
-            "<del>- 1,2,3</del>",
-            "<ins>+ 2,3,4</ins>",
-            "<h3>positive integer</h3>",
-            "<del>- 1</del>",
-            "<ins>+ 3</ins>",
-            "<h3>big integer</h3>",
-            "<del>- -9223372036854775808</del>",
-            "<ins>+ 9223372036854775807</ins>",
-            "<h3>time</h3>",
-            "<del>- 20:15:00</del>",
-            "<ins>+ 19:30:00</ins>",
-            "<h3>date</h3>",
-            "<del>- 1941-05-12</del>",
-            "<ins>+ 2099-12-31</ins>",
-            "<h3>datetime</h3>",
-            "<del>- Aug. 19, 2005, 8:13 a.m.</del>",
-            "<ins>+ Jan. 1, 2000, midnight</ins>",
-            "<h3>decimal</h3>",
-            "<del>- 1.23456789</del>",
-            "<ins>+ 3.1415926535</ins>",
-            "<h3>float</h3>",
-            "<del>- 2.345</del>",
-            "<ins>+ 3.1415</ins>",
-            "<h3>email</h3>",
-            "<del>- one@foo-bar.com</del>",
-            "<ins>+ two@foo-bar.com</ins>",
-            "<h3>url</h3>",
-            """
-            <div class="module">
-                <pre class="highlight">
-                <span class="diff-line diff-del diff-ins">http<ins>s</ins>://
-                <del>www.pylucid.org</del><ins>github.com/jedie</ins>/</span>
-                </pre>
-            </div>
-            """,
-            "<h3>file field</h3>",
-            """
-            <div class="module">
-            <pre class="highlight">
-            <span class="diff-line diff-ins">/media/file_field<ins>_18</ins>.txt</span>
-            </pre>
-            </div>
-            """,
-            "<h3>filepath</h3>",
-            """
-            <div class="module">
-            <pre class="highlight"><del>- foo/</del>
-            <ins>+ bar/</ins></pre>
-            </div>
-            """,
-            "<h3>ip address</h3>",
-            "<del>- 192.168.0.1</del>",
-            "<ins>+ 10.0.0.0</ins>",
+            parts=(
+                "<h3>boolean</h3>",
+                '<p class="highlight"><del>False</del> changed to: <ins>True</ins></p>',
+                "<h3>null boolean</h3>",
+                '<p class="highlight"><del>None</del> changed to: <ins>False</ins></p>',
+                "<h3>char</h3>",
+                "<del>- a</del>",
+                "<ins>+ B</ins>",
+                "<h3>choices char</h3>",
+                "<del>- alpha</del>",
+                "<ins>+ bravo</ins>",
+                "<h3>text</h3>",
+                "<del>- Foo &#39;one&#39;</del>",
+                "<ins>+ Bar &#39;two&#39;</ins>",
+                "<h3>integer</h3>",
+                "<del>- 0</del>",
+                "<ins>+ -1</ins>",
+                "<h3>integers</h3>",
+                "<del>- 1,2,3</del>",
+                "<ins>+ 2,3,4</ins>",
+                "<h3>positive integer</h3>",
+                "<del>- 1</del>",
+                "<ins>+ 3</ins>",
+                "<h3>big integer</h3>",
+                "<del>- -9223372036854775808</del>",
+                "<ins>+ 9223372036854775807</ins>",
+                "<h3>time</h3>",
+                "<del>- 20:15:00</del>",
+                "<ins>+ 19:30:00</ins>",
+                "<h3>date</h3>",
+                "<del>- 1941-05-12</del>",
+                "<ins>+ 2099-12-31</ins>",
+                "<h3>datetime</h3>",
+                "<del>- Aug. 19, 2005, 8:13 a.m.</del>",
+                "<ins>+ Jan. 1, 2000, midnight</ins>",
+                "<h3>decimal</h3>",
+                "<del>- 1.23456789</del>",
+                "<ins>+ 3.1415926535</ins>",
+                "<h3>float</h3>",
+                "<del>- 2.345</del>",
+                "<ins>+ 3.1415</ins>",
+                "<h3>email</h3>",
+                "<del>- one@foo-bar.com</del>",
+                "<ins>+ two@foo-bar.com</ins>",
+                "<h3>url</h3>",
+                """
+                <div class="module">
+                    <pre class="highlight">
+                    <span class="diff-line diff-del diff-ins">http<ins>s</ins>://
+                    <del>www.pylucid.org</del><ins>github.com/jedie</ins>/</span>
+                    </pre>
+                </div>
+                """,
+                "<h3>file field</h3>",
+                """
+                <div class="module">
+                    <pre class="highlight">
+                    <span class="diff-line diff-ins">/media/file_field<ins>_19</ins>.txt</span>
+                    </pre>
+                </div>
+                """,
+                "<h3>filepath</h3>",
+                """
+                <div class="module">
+                    <pre class="highlight"><del>- foo/</del>
+                    <ins>+ bar/</ins></pre>
+                </div>
+                """,
+                "<h3>ip address</h3>",
+                "<del>- 192.168.0.1</del>",
+                "<ins>+ 10.0.0.0</ins>",
+                #
+                '<h3>json field</h3>',
+                '<del>456</del><ins>&#x27;XXX&#x27;</ins>',
+            ),
+        )
+        assert_html_response_snapshot(
+            response,
+            validate=False,
+            query_selector='#content',
+            name_suffix=get_django_name_suffix(),
         )
