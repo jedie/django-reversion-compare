fa05ea8bb5cb3666aa7bcb5345b6b29119b1c05a
JensDiemer <git@jensdiemer.de>
2026-08-03 18:42:07 +0200

    update project

--- manage.py
+++ manage.py
@@ -76,6 +76,11 @@ def main(argv):
         # Activate git pre-commit hooks:
         verbose_check_call(uv_bin, 'run', '--active', '-m', 'pre_commit', 'install')
 
+    # Use always latest versions for local development:
+    if 'NO_AUTO_UV_UPGRADE' not in os.environ:
+        verbose_check_call(uv_bin, 'lock', '--upgrade')
+        verbose_check_call(uv_bin, 'audit')
+
     signal.signal(signal.SIGINT, noop_sigint_handler)  # ignore "Interrupt from keyboard" signals
 
     # Call our entry point CLI:
